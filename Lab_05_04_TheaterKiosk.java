8457f81eafb308c8316861918f5d0278ee9fe277
shane <irvinese@mail.UC.edu>
2022-09-23 12:12:02 -0400

    Irvine_Shane_Lab_05

---
+++ Lab_05_04_TheaterKiosk.java
@@ -0,0 +1,12 @@
+public class Lab_05_04_TheaterKiosk {
+
+    public static void main(String [] args){
+
+        double wristBand = 2;
+        double age = 0;
+        double noWristBand = 1;
+
+        System.out.println("If the persons age is over 21 then:" + age + wristBand);
+        System.out.println("If the persons age is under 21 then" + age + noWristBand);
+    }
+}
