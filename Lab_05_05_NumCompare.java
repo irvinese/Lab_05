87e17fde258085a545e70eba367c45b9e4b1c0ba
shane <irvinese@mail.UC.edu>
2022-09-23 15:35:57 -0400

    Lab_05

---
+++ Lab_05_05_NumCompare.java
@@ -0,0 +1,13 @@
+public class Lab_05_05_NumCompare {
+
+    public static void main(String [] args){
+
+        String equal = "Equal";
+        String different = "Different";
+        String error = "Error";
+
+        System.out.println("if the two numbers are the same then the outcome is:" + equal);
+        System.out.println("If the numbers are differnet then the outcome is:" + different);
+        System.out.println("If the numbers aren't actually numbers then the outcome is:" + error);
+    }
+}
